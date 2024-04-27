9509dc83dd6f4c367b673b557670959e009d7fa5
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-04-27 16:22:02 +0100

    remove fatal flaw from benchmarks

--- sorting/benches/sort.rs
+++ sorting/benches/sort.rs
@@ -1,12 +1,22 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
+use rand::Rng;
+fn generate_random(length: u32, max: u32) -> Vec<u32> {
+    let mut rng = rand::thread_rng();
+    let mut vec: Vec<u32> = Vec::with_capacity(length as usize);
+    for _ in 0..length {
+        let integer = rng.gen_range(0..(max + 1));
+        vec.push(integer)
+    }
+    vec
+}
 fn bench_sorts(c: &mut Criterion) {
     let mut group = c.benchmark_group("Sorts");
+    let random_vector = generate_random(10000, 550);
     group.bench_function("Bubble Sort", |b| {
-        b.iter(|| quick_sort::bubble_sort::bubble_sort(black_box(&mut vec![5, 4, 3, 2, 1])))
+        b.iter(|| quick_sort::bubble_sort::bubble_sort(black_box(&mut random_vector.clone())))
     });
     group.bench_function("Quick Sort", |b| {
-        b.iter(|| quick_sort::quick_sort::quick_sort(black_box(&mut vec![5, 4, 3, 2, 1])))
+        b.iter(|| quick_sort::quick_sort::quick_sort(black_box(&mut random_vector.clone())))
     });
     group.finish();
 }
