25a46ac9d778ad158624999c2e4456a1e99860e8
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-04-27 14:38:41 +0100

    remove log

--- bin_packing/first_fit/src/lib.rs
+++ bin_packing/first_fit/src/lib.rs
@@ -8,7 +8,6 @@ pub fn first_fit(capacity: u32, items: &[u32]) -> Vec<Vec<u32>> {
         let mut fitted = false;
         for b in &mut bins {
             let used_space = b.iter().sum::<u32>();
-            dbg!(used_space);
             if *i + used_space <= capacity {
                 // There is enough space for i to fit into the bin
                 b.push(i.clone());
